4dd27f2a8edf92d69d2cdb7f96154f0e4bc7d07c
Nirupa-4 <dummy@gmail.com>
2026-01-28 17:08:51 +0000

    solve initial commit

--- server/src/main/java/com/edutech/progressive/repository/TicketBookingRepository.java
+++ server/src/main/java/com/edutech/progressive/repository/TicketBookingRepository.java
@@ -1,5 +1,29 @@
 package com.edutech.progressive.repository;
 
+import com.edutech.progressive.entity.TicketBooking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface TicketBookingRepository {
+import javax.transaction.Transactional;
+import java.util.List;
+
+public interface TicketBookingRepository extends JpaRepository<TicketBooking, Integer> {
+
+   
+    List<TicketBooking> findByEmail(String email);
+
+   
+    @Modifying
+    @Transactional
+    @Query("delete from TicketBooking tb where tb.match.matchId = :matchId")
+    void deleteByMatchId(@Param("matchId") int matchId);
+
+
+    @Modifying
+    @Transactional
+    @Query("delete from TicketBooking tb " +
+           "where tb.match.firstTeamId = :teamId or tb.match.secondTeamId = :teamId")
+    void deleteByTeamId(@Param("teamId") int teamId);
 }
